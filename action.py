bcd157159b0384e20dbce113f5c29e3675038183
chigozieokoroafor <okoroaforc14@gmail.com>
2023-06-04 21:09:39 +0100

    working on swapping, but the second to the last index seems to be getting skipped

--- action.py
+++ action.py
@@ -9,6 +9,7 @@ class Bot:
         self.ypos = [-1,-1]
         self.hold = 0
         self.pos = [-1, -1]
+        self.prevPos = [-1, -1]
         
 
     def getPosition(self,x, y):
@@ -36,11 +37,75 @@ class Bot:
         if self.ypos == ypos[1] :
             return(f"Box labelled {y} not found")
         else: return (xpos, ypos)
-        
-    def swap(self, xpos, ypos):
-        pass    
 
-    # def boxOnAnother(self, x, y):
+    def getSinglePosition(self,x):
+        pos = [-1,-1]
+        for row in self.world:
+            # print("================================")    
+            for item in row:
+                # print(item)
+                if item ==  x:
+                    pos[1] = row.index(x)
+                    pos[0] = self.world.index(row)
+                    print(pos)
+        return pos
+
+    def FirstItemInCol(self, col) :
+        pos = [-1,-1]
+        for row in self.world:    
+            if row[col] != 0:
+                pos[1] = col
+                pos[0] = self.world.index(row)
+                return pos
+
+    def swap(self, x, y):
+        # positions = self.getPosition(x, y)    
+        x_pos = self.getSinglePosition(x)
+        y_pos = self.getSinglePosition(y)
+
+        # check which is on top first
+        first = x
+        fPos = x_pos
+        second = y 
+        sPos = y_pos
+        if x_pos[0]> y_pos[0]:
+            first = y
+            fPos = y_pos
+            second = x 
+            sPos = x_pos
+
+        first_clear = self.clear(first)
+
+        while first_clear == False:
+            # row = x_pos[0]
+            column = fPos[1]
+            previousboxpos = self.FirstItemInCol(column)
+            previousBox = self.world[previousboxpos[0]][previousboxpos[1]]
+            # previousbox = self.world[row - 1][column]
+            self.mov(previousBox)
+            self.grab()
+            self.placeOnTable()
+            first_clear = self.clear(first)
+        
+        second_clear = self.clear(second)
+        # print(self.getWorld())
+        print(first, second)
+        print(fPos, sPos)
+        while second_clear == False:
+            # row = x_pos[0]
+            column = sPos[1]
+            previousboxpos = self.FirstItemInCol(column)
+            previousBox = self.world[previousboxpos[0]][previousboxpos[1]]
+            # previousbox = self.world[row - 1][column]
+            self.mov(previousBox)
+            self.grab()
+            self.placeOnTable()
+            second_clear = self.clear(second)
+        
+
+
+
+    # def boxOnAnotherCheck(self, x, y):
     #     pass
 
     # def boxUnderAnother(self, x, y):
@@ -52,11 +117,23 @@ class Bot:
     # def sortDescending(self, x:list):
     #     pass
 
-    def clear(self, x): # this part removes box labelled x
-        pass
+    def clear(self, x): # this part checks if there is any box on top of x
+        # self.mov(x)
+        # self.grab()
+        # self.placeOnTable()
+        current_position = self.getSinglePosition(x)
+        row = current_position[0]
+        column = current_position[1]
 
-    def stack(self, x, y): # this part places box labelled x on
-        pass
+        if self.world[row-1][column] != 0:
+            return False
+        else:
+            return True
+    
+    def stack(self, x, y): # this part places box labelled x on stack
+        self.mov(x)
+        self.grab()
+
             
     def placeOnTable(self):
         column = self.pos[1] - 1
@@ -69,8 +146,13 @@ class Bot:
                 self.world[self.surface[0]][column] = self.hold
                 filled = False
             column += 1
-        
 
+    def placeOnBox(self, y):
+        self.prevPos = self.getSinglePosition(y)
+        row = self.prevPos[0]
+        column = self.prevPos[1]
+        self.world[row-1][column] = self.hold
+        
     def mov(self, x): # this moves to the position of box x
         pos = [-1,-1]
         for row in self.world:    
@@ -89,3 +171,4 @@ class Bot:
         
     def getWorld(self):
         return numpy.array(self.world)
+        
--- world.py
+++ world.py
@@ -4,17 +4,28 @@ world = [
     [0, 0, 0, 0 , 0, 0, 0, 0],
     [0, 0, 0, 0 , 0, 0, 0, 0],
     [0, 0, 0, 0 , 0, 0, 0, 0],
-    [0, 0, 0, 0 , 0, 0, 0, 0],
-    [0, 0, 0, 1 , 0, 0, 0, 0],
-    [0, 0, 0, 3 , 0, 0, 0, 0],
-    [0, 0, 0, 2 , 7, 0, 9, 0],
+    [0, 3, 0, 0, 0 , 0, 0, 0],
+    [0, 4, 0, 0 ,0 , 0, 0, 0],
+    [0, 2, 0, 0 ,0 , 0, 0, 0],
+    [0, 7, 0, 0 ,0 , 0, 0, 0],
 ]
 # print(Bot(world).getPosition(1, 2))
 bot = Bot(world)
-bot.mov(1)
-bot.grab()
-bot.placeOnTable()
+# bot.mov(7)
+# bot.grab()
+# bot.placeOnBox(1)
+# bot.placeOnTable()
+# bot.clear(3)
 print(bot.getWorld())
-
+print("=================================")
+bot.mov(9)
+bot.grab()
+bot.placeOnBox(7)
+(bot.getSinglePosition(2))
+# bot.placeOnTable()
+# print(bot.clear(9))
+# bot.swap(2, 3)
+# print(bot.FirstItemInCol(3))
+# print(bot.getWorld())
 
     
